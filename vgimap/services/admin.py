2e2c90dc7aabd0962c0d46ea0bb52e2233f4d14e
Jeffrey Johnson <jjohnson@opengeo.org>
2012-07-08 02:20:07 -0700

    Added Twitter classes to admin

--- vgimap/services/admin.py
+++ vgimap/services/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.contrib.gis import admin
-from vgimap.services.models import Event, Service, UshahidiCategory, UshahidiReport
+from vgimap.services.models import Event, Service
+from vgimap.services.models import UshahidiCategory, UshahidiReport
+from vgimap.services.models import TwitterPlace, TwitterUser, TwitterUrl, TwitterTweet, TwitterHashtag
 
 admin.site.register(Event,admin.OSMGeoAdmin)
 admin.site.register(Service)
 admin.site.register(UshahidiCategory)
 admin.site.register(UshahidiReport, admin.OSMGeoAdmin)
+admin.site.register(TwitterPlace, admin.OSMGeoAdmin)
+admin.site.register(TwitterTweet, admin.OSMGeoAdmin)
+admin.site.register(TwitterUser)
+admin.site.register(TwitterUrl)
+admin.site.register(TwitterHashtag)
