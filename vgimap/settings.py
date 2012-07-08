c5087d95fd8dd930666519e3f1a9fbf9e30217ba
Jeffrey Johnson <jjohnson@opengeo.org>
2012-07-07 20:35:56 -0700

    Change default database credentials

--- vgimap/settings.py
+++ vgimap/settings.py
@@ -14,9 +14,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'geodb',                      # Or path to database file if using sqlite3.
-        'USER': 'geodb',                      # Not used with sqlite3.
-        'PASSWORD': 'geodb',                  # Not used with sqlite3.
+        'NAME': 'vgimap',                      # Or path to database file if using sqlite3.
+        'USER': 'vgimap',                      # Not used with sqlite3.
+        'PASSWORD': 'vgimap',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
