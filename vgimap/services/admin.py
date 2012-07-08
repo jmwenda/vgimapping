1c1b4e0c4f23e5ac6f63307d7c605863f695ea11
Jeffrey Johnson <jjohnson@opengeo.org>
2012-07-07 23:33:14 -0700

    Added new classes to Admin

--- vgimap/services/admin.py
+++ vgimap/services/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.contrib.gis import admin
-from vgimap.services.models import event
+from vgimap.services.models import Event, Service, UshahidiCategory, UshahidiReport
 
-admin.site.register(event,admin.GeoModelAdmin)
+admin.site.register(Event,admin.OSMGeoAdmin)
+admin.site.register(Service)
+admin.site.register(UshahidiCategory)
+admin.site.register(UshahidiReport, admin.OSMGeoAdmin)
