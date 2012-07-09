c022b53383f30795ed5f37659f73741f21caadd2
Jude Mwenda <judemwenda@gmail.com>
2012-07-09 16:55:48 +0300

    Added the ushahidi api json object, next is saving to the database

--- vgimap/services/management/commands/fetchservices.py
+++ vgimap/services/management/commands/fetchservices.py
@@ -1,24 +1,37 @@
 from django.core.management.base import BaseCommand
 from optparse import make_option
-from vgimap.services.models import Service
+from vgimap.services.models import Service,UshahidiReport,UshahidiCategory
 import requests
+from urlparse import urljoin
+import json
 
-def get_response(service):
+extract = lambda keys, dict: reduce(lambda x, y: x.update({y[0]:y[1]}) or x,
+                                            map(None, keys, map(dict.get, keys)), {})
+
+def get_response(url):
     "This hits the api identified by the service and returns the response"
     try:
-	req = requests.get(service.url)
+	req = requests.get(url)
         return req
     except requests.exceptions.ConnectionError, e:
         return e 
 def fetch_service():
-    #we get a list of services  to query,for a start just twitter, will add the other services as we progress
+    #we get a list of services  to query,for a start just ushahidi, will add the other services as we progress
     #services = Service.objects.all()
-    services = Service.objects.filter(type='TWT')
+    services = Service.objects.filter(type='USH')
     service_response = []
     for service in services:
-       response = get_response(service)
-       if response is not None:
-           service_response.append(response)
+        if service.type == 'USH':
+            url = urljoin(service.url,'api?task=incidents') 
+            response = get_response(url)
+            data = response.json
+            indicents = data['payload']['incidents']
+            for incident in indicents:
+               #to get the incident details use the dict 'incident' and categories use 'categories'
+               print incident
+               
+            if response is not None:
+                service_response.append(response)
     return service_response
 
 class Command(BaseCommand):
