7eea6cd0911e5a968e317e6959a614f7d2b395d4
Jeffrey Johnson <jjohnson@opengeo.org>
2012-07-20 14:50:10 -0700

    Add get_feature_descriptions body, remove get_feature_description

--- vgimap/services/wfs.py
+++ vgimap/services/wfs.py
@@ -1,14 +1,25 @@
-from ga_ows.views.wfs import WFSAdapter
+from ga_ows.views.wfs import WFSAdapter, FeatureDescription
 
 class TwitterWFSAdapter(WFSAdapter):
     def __init__(self):
         pass
 
     def get_feature_descriptions(self, request, **params):
-        pass
+        namespace = request.build_absolute_uri().split('?')[0] + "/schema"
+        extent = (-180,-90,180,90)
 
-    def get_feature_description(self, request, **types):
-        pass
+        return [FeatureDescription(
+                ns=namespace,
+                ns_name="tweets",
+                name="tweets",
+                abstract="twitter tweets",
+                title="twitter tweets",
+                keywords=[],
+                srs="EPSG:4326",
+                bbox=[-180,-90,180,90],
+                schema=namespace
+            )]     
+
 
     def list_stored_queries(self, request):
         pass
