12b68f8212d2310ead91a7bfde245a478b95219f
Jeffrey Johnson <jjohnson@opengeo.org>
2012-07-20 14:09:41 -0700

    Initial stub of TwitterWFSAdapter class

---
+++ vgimap/services/wfs.py
@@ -0,0 +1,17 @@
+from ga_ows.views.wfs import WFSAdapter
+
+class TwitterWFSAdapter(WFSAdapter):
+    def __init__(self):
+        pass
+
+    def get_feature_descriptions(self, request, **params):
+        pass
+
+    def get_feature_description(self, request, **types):
+        pass
+
+    def list_stored_queries(self, request):
+        pass
+
+    def get_features(self, request, params):
+        pass
