270295dc760ec274afa41047fd6fa5b7c8337d28
Jeffrey Johnson <jjohnson@opengeo.org>
2012-07-22 22:34:56 -0700

    Added new OSM classes to admin

--- vgimap/services/admin.py
+++ vgimap/services/admin.py
@@ -3,6 +3,7 @@ from django.contrib.gis import admin
 from vgimap.services.models import Event, Service
 from vgimap.services.models import UshahidiCategory, UshahidiReport
 from vgimap.services.models import TwitterPlace, TwitterUser, TwitterUrl, TwitterTweet, TwitterHashtag
+from vgimap.services.models import OsmNode, OsmNodeTag, OsmWay
 
 admin.site.register(Event,admin.OSMGeoAdmin)
 admin.site.register(Service)
@@ -13,3 +14,7 @@ admin.site.register(TwitterTweet, admin.OSMGeoAdmin)
 admin.site.register(TwitterUser)
 admin.site.register(TwitterUrl)
 admin.site.register(TwitterHashtag)
+admin.site.register(OsmNode, admin.OSMGeoAdmin)
+admin.site.register(OsmNodeTag)
+admin.site.register(OsmWay, admin.OSMGeoAdmin)
+admin.site.register(OsmWayTag)
