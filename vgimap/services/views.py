d1c89c7439676aff6f88df620d0f0462f82c4b77
Jude Mwenda <judemwenda@gmail.com>
2012-07-27 12:24:24 +0300

    Changed from render to response to be able to pass the context

--- vgimap/services/views.py
+++ vgimap/services/views.py
@@ -1,4 +1,9 @@
 # Create your views here
-from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from django.template import Context, loader
 def opensearch(request):
-  return render_to_response('opensearch.xml')
+    response = HttpResponse(mimetype='application/opensearchdescription+xml')
+    t = loader.get_template('opensearch.xml')
+    c = Context({})
+    response.write(t.render(c))
+    return response
