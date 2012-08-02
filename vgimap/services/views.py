3eca48860c863278bf1a63b9d16ef49c71e9ede9
Jude Mwenda <judemwenda@gmail.com>
2012-08-02 19:17:07 +0300

    Changes to lat/long radius querying

--- vgimap/services/views.py
+++ vgimap/services/views.py
@@ -2,6 +2,7 @@
 from django.http import HttpResponse
 from django.template import Context, loader
 import OsmApi
+import math
 from lxml import etree
 import urllib
 
@@ -39,7 +40,25 @@ def open_search(data):
         entry.append(point)
         feed.append(entry)
     return root
-    
+
+def deg2rad(degrees):
+    pi = math.pi
+    radians = pi * degrees / 180
+    return radians
+
+def rad2deg(radians):
+    pi = math.pi
+    degrees = 180 * radians / pi
+    return degrees
+
+def get_boundingbox(lat,lon,radius):
+    earth_radius = 6371;
+    maxLat = lat + rad2deg(float(radius)/float(earth_radius));
+    minLat = lat - rad2deg(radius/earth_radius);
+    maxLon = lon + rad2deg(radius/earth_radius/math.cos(deg2rad(lat)));
+    minLon = lon - rad2deg(radius/earth_radius/math.cos(deg2rad(lat)));
+    return bbox
+
 def search_osm(search_criteria):
     #we do need to have some bbox validation
     if search_criteria['bbox'] is not None:
@@ -47,6 +66,9 @@ def search_osm(search_criteria):
     elif search_criteria['radius'] is not None:
         #need to confirm a coulpe of aspects about this,especially radius querying
         fetch_path = "/api/interpreter?data=node[name~'"+ search_criteria['search_term']+"'](around:"+ search_criteria['radius']+");out;"
+        if search_criteria['lat'] and search_criteria['lon'] is not None:
+            bbox = get_boundingbox(search_criteria['lat'],search_criteria['lon'],search_criteria['radius']) 
+            fetch_path = "/api/interpreter?data=node[name~'"+ search_criteria['search_term']+"'](around:"+ bbox +");out;"
     else:
         fetch_path = "/api/interpreter?data=node[name~'"+ search_criteria['search_term']+"'];out;"
     url = urllib.quote(fetch_path,'?/=')
@@ -62,6 +84,8 @@ def search(request):
     search_criteria ['search_term'] = request.GET.get('q', '')
     search_criteria ['bbox'] = request.GET.get('bbox')
     search_criteria ['radius'] = request.GET.get('radius')
+    search_criteria ['lat'] = request.GET.get('lat')
+    search_criteria ['lon'] = request.GET.get('lon')
     #perfrom search and return results set from the different services
     osm_results = search_osm(search_criteria)
     #we get a json dataset that needs to be made into opengeosearch capable
