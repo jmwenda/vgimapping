1acda5a66f2bbe76267ab98818d611edfc6d9f2b
Jude Mwenda <judemwenda@gmail.com>
2012-08-31 17:22:26 +0300

    Settings file clean up

--- vgimap/settings.py
+++ vgimap/settings.py
@@ -103,13 +103,6 @@ MIDDLEWARE_CLASSES = (
 )
 
 
-HAYSTACK_CONNECTIONS = {
-    'default': {
-        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
-        'URL': 'http://127.0.0.1:9200/',
-        'INDEX_NAME': 'haystack',
-    },
-}
 ROOT_URLCONF = 'vgimap.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
